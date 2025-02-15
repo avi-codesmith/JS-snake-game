24f4093c449ed7fbcfaba6fe2e68b4091eac2ba5
avi-codesmith <avi.codesmith@gmail.com>
2025-02-15 20:32:22 +0530

    JS Snake Game

--- script.js
+++ script.js
@@ -1,51 +1,101 @@
 "use strict";
 
-const playbground = document.querySelector(".playground");
+const playground = document.querySelector(".playground");
+let scoreEl = document.querySelector(".numberScore");
+let highScoreEl = document.querySelector(".numberHighScore");
 
-let foodX = null;
-let foodY = null;
+let foodX, foodY;
 let headX = Math.floor(Math.random() * 30) + 1;
 let headY = Math.floor(Math.random() * 30) + 1;
-let valocityX = 0;
-let valocityY = 0;
+let velocityX = 0;
+let velocityY = 0;
 let snakeBody = [];
+let gameover = false;
+let eatSound = new Audio("./eat.mp3");
+let loseSound = new Audio("./lose.mp3");
+let gameloop;
+let score = 0;
+
+let highScore = localStorage.getItem("highScore") || 0;
+highScoreEl.innerHTML = highScore;
 
 const random = () => {
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
 };
 
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp" && velocityY === 0) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown" && velocityY === 0) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft" && velocityX === 0) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight" && velocityX === 0) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+});
+
 const initGame = () => {
   let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX};"></div>`;
-  headX = headX + valocityX;
-  headY = headY + valocityY;
-  htmlMarkup += `<div class="head" style="grid-area: ${headY} / ${headX};"></div>`;
-  playbground.innerHTML = htmlMarkup;
+
+  headX += velocityX;
+  headY += velocityY;
+
   if (headX === foodX && headY === foodY) {
     random();
-    snakeBody.push([foodX, foodY]);
-    console.log(snakeBody);
+    snakeBody.push([headX, headY]);
+    eatSound.play();
+    score++;
+    scoreEl.innerHTML = score;
+
+    if (score > highScore) {
+      highScore = score;
+      localStorage.setItem("highScore", highScore);
+      highScoreEl.innerHTML = highScore;
+    }
+  }
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  snakeBody[0] = [headX, headY];
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    let className = i === 0 ? "head mouth" : "head";
+    htmlMarkup += `<div class="${className}" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]};"></div>`;
+
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver();
+    }
+  }
+
+  playground.innerHTML = htmlMarkup;
+
+  if (headX <= 0 || headX > 30 || headY <= 0 || headY > 30) {
+    gameOver();
   }
 };
 
-const changeDirection = document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp") {
-    valocityX = 0;
-    valocityY = -1;
-  } else if (e.key === "ArrowDown") {
-    valocityX = 0;
-    valocityY = 1;
-  } else if (e.key === "ArrowRight") {
-    valocityX = 1;
-    valocityY = 0;
-  } else if (e.key === "ArrowLeft") {
-    valocityX = -1;
-    valocityY = 0;
-  }
-  initGame();
-});
+const gameOver = () => {
+  playground.style.backgroundColor = "#fa5252";
+  gameover = true;
+  loseSound.play();
+  clearInterval(gameloop);
+
+  setTimeout(() => {
+    alert("You lose! Click OK to play again.");
+    location.reload();
+  }, 100);
+};
 
 random();
-setInterval(() => {
-  initGame();
-}, 222);
+gameloop = setInterval(initGame, 150);
